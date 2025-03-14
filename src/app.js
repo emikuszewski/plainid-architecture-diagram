915c7256298bee530895fcb0f08386a4e2f68e2a
Ed Mikuszewski <emikuszewski@pivotal.io>
2025-03-14 12:36:09 -0500

    Create app.js

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ArchitectureDiagram from './components/ArchitectureDiagram';
+
+function App() {
+  return (
+    <div className="App">
+      <ArchitectureDiagram />
+    </div>
+  );
+}
+
+export default App;
